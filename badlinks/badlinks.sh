1e09a2a4a71e42c6001ebf4d68eab638684e894c
svetomsk <svt2404@gmail.com>
2016-03-29 01:43:08 +0300

    Third homework implemented.

---
+++ badlinks/badlinks.sh
@@ -0,0 +1,20 @@
+scan() {
+    for e in "$1"/*; do
+        if [ -d "$e" -a ! -h "$e" ]
+        then
+            scan "$e"
+        elif [ -h "$e" -a -e "$e" ]
+        then
+            time=$(stat -c %Y $e)
+            cur=$(date +%s)
+            diff=$(($cur - $time))
+            if [ $diff -ge 10080000 ] 
+            then
+                echo "$e"
+            fi 
+        fi
+    done
+}
+
+scan "$1"
+
